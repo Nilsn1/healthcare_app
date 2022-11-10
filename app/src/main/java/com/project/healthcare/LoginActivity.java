33584a0c80ac0e1aaed23ce40e27376c4ef295e3
admin <nileshsonawanen1@gmail.com>
2022-11-10 16:53:19 +0530

    firebase authentication

--- app/src/main/java/com/project/healthcare/LoginActivity.java
+++ app/src/main/java/com/project/healthcare/LoginActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText email, password;
+    Button btnLogin;
     TextView btnSignup;
 
     @Override
@@ -17,7 +20,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.btnLogin);
         btnSignup = findViewById(R.id.btnSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
 
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/project/healthcare/SignUpActivity.java
+++ app/src/main/java/com/project/healthcare/SignUpActivity.java
@@ -1,24 +1,52 @@
 package com.project.healthcare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    TextView btnLogin;
+    EditText email, password;
+    Button btnsignup;
+    TextView login;
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        btnLogin = findViewById(R.id.btnLogin);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btnsignup = findViewById(R.id.signup);
+        login = findViewById(R.id.btnLogin);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+
+                signupFirebase(userEmail,userPassword);
+
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -28,6 +56,28 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void signupFirebase(String userEmail, String userPassword) {
+
+        auth.createUserWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(SignUpActivity.this, "Your account has been Created", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Error occured", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
 
     }
 }
