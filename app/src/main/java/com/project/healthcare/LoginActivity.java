3895536ecb270389f66a95e93809c39eeab1670e
admin <nileshsonawanen1@gmail.com>
2022-11-10 17:17:16 +0530

    firebase authentication

--- app/src/main/java/com/project/healthcare/LoginActivity.java
+++ app/src/main/java/com/project/healthcare/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.project.healthcare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,12 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText email, password;
     Button btnLogin;
     TextView btnSignup;
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,10 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+
+                loginFirebase(userEmail,userPassword);
 
             }
         });
@@ -42,6 +55,29 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    public void loginFirebase(String userEmail, String userPassword) {
+
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+//                            Toast.makeText(LoginActivity.this, "Your account has been Created", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Please enter correct email and password", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
 
     }
 }
